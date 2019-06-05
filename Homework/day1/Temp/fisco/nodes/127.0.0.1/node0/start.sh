f9935818255dbd2d37e175f4b3278b5e9b840c8a
GyDi <segydi@foxmail.com>
2019-06-05 11:00:52 +0800

    add fisco files

---
+++ Homework/day1/Temp/fisco/nodes/127.0.0.1/node0/start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+SHELL_FOLDER=$(cd $(dirname $0);pwd)
+
+fisco_bcos=${SHELL_FOLDER}/../fisco-bcos
+cd ${SHELL_FOLDER}
+node=$(basename ${SHELL_FOLDER})
+node_pid=`ps aux|grep ${fisco_bcos}|grep -v grep|awk '{print $2}'`
+if [ ! -z ${node_pid} ];then
+    echo " ${node} is running, pid is $node_pid."
+    exit 0
+else 
+    nohup ${fisco_bcos} -c config.ini 2>>nohup.out &
+    sleep 1.5
+fi
+try_times=4
+i=0
+while [ $i -lt ${try_times} ]
+do
+    node_pid=`ps aux|grep ${fisco_bcos}|grep -v grep|awk '{print $2}'`
+    if [ ! -z ${node_pid} ];then
+        echo -e "\033[32m ${node} start successfully\033[0m"
+        exit 0
+    fi
+    sleep 0.5
+    ((i=i+1))
+done
+echo -e "\033[31m  Exceed waiting time. Please try again to start ${node} \033[0m"
+cat nohup.out
+exit 1
